3ce1de3c71435d60c329592c81b9683c0c02618e
dmitrgulyaeff <dmitrgulyaeff@yandex.ru>
2023-07-11 08:49:25 +0700

    добавить: проброс класса в Toggle

--- src/components/Toggle/Toggle.tsx
+++ src/components/Toggle/Toggle.tsx
@@ -3,14 +3,15 @@ import classNames from 'classnames'
 
 
 interface ToggleProps {
+  className: string,
   enabled: boolean,
   toggle: () => void
 }
 
-export default function Toggle({enabled, toggle} : ToggleProps) {
+export default function Toggle({enabled, toggle, className} : ToggleProps) {
   
     return (
-      <div className={classNames('toggle', {'toggle_active': enabled})} onClick={toggle}>
+      <div className={classNames('toggle', {'toggle_active': enabled}) + ` ${className}`} onClick={toggle}>
         <div className={classNames('toggle__circle', {'toggle__circle_active': enabled})} />
       </div>
     )
