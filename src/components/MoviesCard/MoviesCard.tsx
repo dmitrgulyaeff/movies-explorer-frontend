2b61f5df1c842cb4f69db6c66ee95bd5c4edbeae
dmitrgulyaeff <dmitrgulyaeff@yandex.ru>
2023-07-13 02:19:30 +0700

    добавить: стили MoviesCardList

--- src/components/MoviesCard/MoviesCard.tsx
+++ src/components/MoviesCard/MoviesCard.tsx
@@ -1,13 +1,38 @@
+import './MoviesCard.css'
+
+import { User } from '../../utils/types';
 import { Movie } from '../../utils/types';
+import classNames from 'classnames';
+import { Link } from 'react-router-dom';
 
-interface MoviesCardProps extends Movie {}
+interface MoviesCardProps {
+  movie: Movie,
+  user?: User,
+  pathname: string
+}
 
-export default function MoviesCard({image, nameRU, duration, movieId} : MoviesCardProps) {
+const minutesToString = ( minutes: number) => {
+  const hh = Math.floor(minutes / 60);
+  const mm = minutes % 60;
+  return `${!!hh ? (hh + 'ч') : ''}${mm + 'м'}`
+}
+
+export default function MoviesCard({movie, user, pathname} : MoviesCardProps) {
+  const {image, nameRU, duration, movieId, owner, trailerLink} = movie;
+  const btnClass = classNames(
+    {'movie__btn-like': pathname === '/movies'}, 
+    {'movie__btn-like_active':  user && pathname === '/movies' && (user._id === owner._id)},
+    {'movie__btn-cross': user && pathname === '/saved-movies'}
+  )
+
   return (
-    <article key={movieId.toString()} >
-      <img src={image} alt={nameRU} />
-      <h4>{nameRU}</h4>
-      <p>{duration}</p>
+    <article className='movie' key={movieId.toString()} >
+      <Link to={trailerLink} target="_blank">
+      <img className='movie__image' src={image} alt={nameRU} />
+      </Link>
+      <h4 className='movie__name'>{nameRU}</h4>
+      <p className='movie__duration'>{minutesToString(duration)}</p>
+      <button className={btnClass}/>
     </article>
   );
 }
--- src/components/MoviesCardList/MoviesCardList.tsx
+++ src/components/MoviesCardList/MoviesCardList.tsx
@@ -1,19 +1,22 @@
+import './MoviesCardList.css'
+
 import MoviesCard from '../MoviesCard/MoviesCard';
 import Preloader from '../Preloader/Preloader';
 
-import { MoviesContext, PathnameContext } from '../../Contexts';
+import { MoviesContext, PathnameContext, CurrentUserContext } from '../../Contexts';
 import { useContext } from 'react';
 
 export default function MoviesCardList() {
   const { savedMovies, yaMovies } = useContext(MoviesContext);
   const { pathname } = useContext(PathnameContext);
+  const { user } = useContext(CurrentUserContext)
 
   return (
-    <section>
+    <section className='movies'>
       {pathname === '/movies' &&
-        (yaMovies ? yaMovies.map(MoviesCard) : <Preloader />)}
+        (yaMovies ? yaMovies.map((movie) => MoviesCard({movie, user, pathname})) : <Preloader />)}
       {pathname === '/saved-movies' &&
-        (savedMovies ? savedMovies.map(MoviesCard) : <Preloader />)}
+        (savedMovies ? savedMovies.map((movie) => MoviesCard({movie, user, pathname})) : <Preloader />)}
     </section>
   );
 }
