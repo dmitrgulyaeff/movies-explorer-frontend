d4f9dffce9b677b7cb0cf6e3248338ccb27b1a00
dmitrgulyaeff <dmitrgulyaeff@yandex.ru>
2023-09-12 11:44:07 +0700

    исправить: бургер только при авторизации

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,22 +6,20 @@ import Burger from '../Burger/Burger';
 import { AuthorizedContext, PathnameContext } from '../../Contexts';
 import { useContext } from 'react';
 
-
 export default function Header() {
-  const {isAuthorized} = useContext(AuthorizedContext)
+  const { isAuthorized } = useContext(AuthorizedContext);
 
   const { pathname } = useContext(PathnameContext);
   const allowedPaths: string[] = ['/', '/movies', '/saved-movies', '/profile'];
   const protectedPaths = ['/movies', '/saved-movies', '/profile'];
 
   if (!allowedPaths.includes(pathname)) {
-    return null; 
+    return null;
   }
 
   if (protectedPaths.includes(pathname) && !isAuthorized) {
-    return null; 
+    return null;
   }
-
 
   const savedFilmsStylesClass = classNames(
     'navbar__link navbar__link_to_movies',
@@ -35,20 +33,22 @@ export default function Header() {
   });
 
   return (
-    <header className="header">
-      <nav className='navbar'>
+    <header
+      className={classNames('header', { header_authorized: isAuthorized })}
+    >
+      <nav className="navbar">
         <ul className="navbar__ul">
           <li className="navbar__li navbar__li_to_root">
             <Link className="navbar__link navbar__link_to_root" to="/"></Link>
           </li>
           {isAuthorized ? (
             <>
-              <li className="navbar__li">
+              <li className="navbar__li navbar__li_to_movies">
                 <Link className={filmsStylesClass} to="/movies">
                   Фильмы
                 </Link>
               </li>
-              <li className="navbar__li">
+              <li className="navbar__li navbar__li_to_saved-movies">
                 <Link className={savedFilmsStylesClass} to="/saved-movies">
                   Сохранённые фильмы
                 </Link>
@@ -84,7 +84,7 @@ export default function Header() {
           )}
         </ul>
       </nav>
-      <Burger/>
+      {isAuthorized && <Burger />}
     </header>
   );
 }
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,5 +1,9 @@
 import './Navigation.css';
-import { PopupContext, AuthorizedContext, PathnameContext } from '../../Contexts';
+import {
+  PopupContext,
+  PathnameContext,
+  AuthorizedContext,
+} from '../../Contexts';
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames';
@@ -7,63 +11,74 @@ import classNames from 'classnames';
 interface NavigationProps {}
 
 export default function Navigation(props: NavigationProps) {
-  const {isPopupOpened, setPopupOpened } = useContext(PopupContext);
-  const {isAuthorized } = useContext(AuthorizedContext);
-  
+  const { isPopupOpened, setPopupOpened } = useContext(PopupContext);
+  const { isAuthorized } = useContext(AuthorizedContext);
   const { pathname } = useContext(PathnameContext);
 
-  const rootStylesClass = classNames('aside__nav-link', {'aside__nav-link_active' : pathname === '/'})
-  const moviesStylesClass = classNames('aside__nav-link', {'aside__nav-link_active' : pathname === '/movies'})
-  const savedMoviesStylesClass = classNames('aside__nav-link', {'aside__nav-link_active' : pathname === '/saved-movies'})
+  const rootStylesClass = classNames('aside__nav-link', {
+    'aside__nav-link_active': pathname === '/',
+  });
+  const moviesStylesClass = classNames('aside__nav-link', {
+    'aside__nav-link_active': pathname === '/movies',
+  });
+  const savedMoviesStylesClass = classNames('aside__nav-link', {
+    'aside__nav-link_active': pathname === '/saved-movies',
+  });
 
-
-  const signinStylesClass = classNames('aside__nav-link', {'aside__nav-link_active' : pathname === '/signin'})
-  const signupStylesClass = classNames('aside__nav-link', {'aside__nav-link_active' : pathname === '/signup'})
-
-  const closeHandler = () => {setPopupOpened(false)}
+  const closeHandler = () => {
+    setPopupOpened(false);
+  };
   return (
-    <section className={classNames("popup", {popup_opened: isPopupOpened})} >
-      <aside className={classNames("aside", {aside_opened: isPopupOpened})}>
-        <nav className="aside__nav">
-          <ul className="aside__ul">
-            {isAuthorized ? 
-            <>
-              <li className="aside__li">
-                <Link onClick={closeHandler} className={rootStylesClass} to={'/'}>
-                  Главная
-                </Link>
-              </li>
-              <li className="aside__li">
-                <Link onClick={closeHandler} className={moviesStylesClass} to={'/movies'}>
-                  Фильмы
-                </Link>
-              </li>
-              <li className="aside__li">
-                <Link onClick={closeHandler} className={savedMoviesStylesClass} to={'/saved-movies'}>
-                  Сохранённые фильмы
-                </Link>
-              </li>
-              <li className="aside__li aside__li_to_profile">
-                <Link onClick={closeHandler} className="aside__nav-link aside__nav-link_to_profile" to={'/profile'} />
-              </li>
-            </>: 
-            <>
-               <li className="aside__li">
-                <Link onClick={closeHandler} className={signinStylesClass} to={'/signin'}>
-                Войти
-                </Link>
-              </li>
-              <li className="aside__li">
-                <Link onClick={closeHandler} className={signupStylesClass} to={'/signup'}>
-                Регистрация
-                </Link>
-              </li>
-             
-            </>
-            }
-          </ul>
-        </nav>
-      </aside>
-    </section>
+    <>
+      {' '}
+      {isAuthorized && (
+        <section
+          className={classNames('popup', { popup_opened: isPopupOpened })}
+        >
+          <aside
+            className={classNames('aside', { aside_opened: isPopupOpened })}
+          >
+            <nav className="aside__nav">
+              <ul className="aside__ul">
+                <li className="aside__li">
+                  <Link
+                    onClick={closeHandler}
+                    className={rootStylesClass}
+                    to={'/'}
+                  >
+                    Главная
+                  </Link>
+                </li>
+                <li className="aside__li">
+                  <Link
+                    onClick={closeHandler}
+                    className={moviesStylesClass}
+                    to={'/movies'}
+                  >
+                    Фильмы
+                  </Link>
+                </li>
+                <li className="aside__li">
+                  <Link
+                    onClick={closeHandler}
+                    className={savedMoviesStylesClass}
+                    to={'/saved-movies'}
+                  >
+                    Сохранённые фильмы
+                  </Link>
+                </li>
+                <li className="aside__li aside__li_to_profile">
+                  <Link
+                    onClick={closeHandler}
+                    className="aside__nav-link aside__nav-link_to_profile"
+                    to={'/profile'}
+                  />
+                </li>
+              </ul>
+            </nav>
+          </aside>
+        </section>
+      )}
+    </>
   );
 }
