adf4eada17884dd8ba0d160fecd18de66a304a6d
dmitrgulyaeff <dmitrgulyaeff@yandex.ru>
2023-09-12 10:58:56 +0700

    исправить: фиксировать ширину root на 1280px

--- src/utils/getCardsCountForMore.ts
+++ src/utils/getCardsCountForMore.ts
@@ -2,6 +2,5 @@ export const getCardsCountForMore = (width: number) => {
   if (width < 500) return 2;
   if (width < 900) return 2;
   if (width < 1100) return 3;
-  if (width < 1400) return 4;
-  return 5;
+  return 4;
 };
--- src/utils/getInitialCardsCount.ts
+++ src/utils/getInitialCardsCount.ts
@@ -1,6 +1,5 @@
 export  const getInitialCardsCount = (width: number) => {
   if (width < 900) return 2 * 4;
   if (width < 1100) return 3 * 4;
-  if (width < 1400) return 4 * 4;
-  return 5 * 3;
+  return 4 * 4;
 };
